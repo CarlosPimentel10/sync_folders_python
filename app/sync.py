8c1ce850028672fa8e7f3b9e8db375d345624774
unknown <carlos.costa.pimentel@gmail.com>
2023-11-09 15:59:34 +0000

    refactored main functions

--- app/sync.py
+++ app/sync.py
@@ -2,25 +2,59 @@ import os
 import argparse
 import logging
 import time
+import hashlib
+import shutil
 
-def folder_sync(source, replica, log, time_frame):
-    # check if directory exists
-    if not os.path.isdir(replica):
-        os.makedirs(replica)
-    if not os.path.source.isdir(source):
-        raise argparse.ArgumentError(None, "The source folder doesn't exist.")
+def create_folder_if_not_exists(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+def compare_files(file1, file2):
+    with open(file1, "rb") as f1, open(file2, "rb") as f2:
+        return hashlib.md5(f1.read()).hexdigest() == hashlib.md5(f2.read()).hexdigest()
+
+def copy_or_update_file(source_path, replica_path):
+    if os.path.exists(replica_path):
+        if compare_files(source_path, replica_path):
+            return f'{source_path} is already up to date.'
+        else:
+            os.remove(replica_path)
+            shutil.copy2(source_path, replica_path)
+            return f'{source_path} has been updated.'
+    else:
+        shutil.copy2(source_path, replica_path)
+        return f'{source_path} has been copied to the replica.'
+
+def folder_sync(source_folder, replica_folder, log_file, time_interval):
+    create_folder_if_not_exists(replica_folder)
     
-    logging.info(f"Start with parameters:\nsource:{source}\nreplica:{replica}\nlog:{log}\ntime_interval:{time_frame}\n")
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s')
     
     while True:
         try:
-            folder_comparison(source, replica)
-            time.sleep(time_frame)
+            logging.info(f"Start synchronization from {source_folder} to {replica_folder} every {time_interval} seconds.")
+            source_files = os.listdir(source_folder)
+            replica_files = os.listdir(replica_folder)
+            
+            for file in source_files:
+                source_file_path = os.path.join(source_folder, file)
+                replica_file_path = os.path.join(replica_folder, file)
+                
+                log_message = copy_or_update_file(source_file_path, replica_file_path)
+                logging.info(log_message)
+                print(log_message)
+            
+            time.sleep(time_interval)
         except KeyboardInterrupt:
-            print("The Program is terminated manually!")
+            print("The program has been terminated manually.")
             raise SystemExit
-        
-        def folder_comparison():
-            pass
-        
-        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Folder synchronization program')
+    parser.add_argument('--source', type=str, default="source", help='Source folder path')
+    parser.add_argument('--replica', type=str, default="replica", help='Replica folder path')
+    parser.add_argument('--log-file', type=str, default='log.txt', help='Log file path')
+    parser.add_argument('--time-interval', type=int, default=60, help='Time interval for synchronization in seconds')
+    args = parser.parse_args()
+
+    folder_sync(args.source, args.replica, args.log_file, args.time_interval)
--- app/tests.py
+++ app/tests.py
@@ -1,21 +1,75 @@
-import os 
-from sync import folder_sync
+import os
+import pytest
+import shutil
 
-def test_folder_sync():
-    source = 'source_folder'
-    replica = 'replica_folder'
-    log = 'log_file'
-    time_frame = 5
+from sync import folder_sync,folder_comparison, file_comparison
 
-    os.makedirs(source, exist_ok=True)
-    with open(os.path.join(source, 'test.txt'), 'w') as f:
-        f.write("LOREM IPSUM LOREM IPSUM")
-    try:
-        folder_sync(source=source, replica=replica, log=log, time_frame=time_frame)
+# Create a temporary test directory
+@pytest.fixture(scope="module")
+def test_directory(tmp_path_factory):
+    temp_dir = tmp_path_factory.mktemp("test_directory")
+    yield temp_dir
+    shutil.rmtree(temp_dir)
 
-        assert os.path.isdir(replica)
-        assert os.path.isfile(os.path.join(replica, 'test_file.txt'))
-    finally:
-        os.remove(os.path.join(source, 'test.txt'))
-        os.rmdir(source)
-        os.rmdir(replica)
+def test_folder_sync_create_folder(test_directory):
+    source = os.path.join(test_directory, "source")
+    replica = os.path.join(test_directory, "replica")
+    log = os.path.join(test_directory, "log.txt")
+    time_frame = 1
+
+    # Create the source folder within the test directory
+    os.makedirs(source)
+
+    folder_sync(source, replica, log, time_frame)
+
+    assert os.path.isdir(source)
+    assert os.path.isdir(replica)
+    assert os.path.isfile(log)
+
+def test_file_comparison():
+    file1 = "test_file1.txt"
+    file2 = "test_file2.txt"
+    file3 = "test_file3.txt"
+
+    with open(file1, "w") as f:
+        f.write("content")
+
+    with open(file2, "w") as f:
+        f.write("content")
+
+    with open(file3, "w") as f:
+        f.write("different_content")
+
+    assert file_comparison(file1, file2)  # Identical files
+    assert not file_comparison(file1, file3)  # Different files
+
+def test_folder_comparison(tmp_path):
+    test_directory = tmp_path
+    source = os.path.join(test_directory, "source")
+    replica = os.path.join(test_directory, "replica")
+
+    os.makedirs(source)
+    os.makedirs(replica)
+
+    # Create test files
+    source_file1 = os.path.join(source, "file1.txt")
+    replica_file1 = os.path.join(replica, "file1.txt")
+    with open(source_file1, "w") as f:
+        f.write("content")
+
+    source_file2 = os.path.join(source, "file2.txt")
+    replica_file2 = os.path.join(replica, "file2.txt")
+    with open(source_file2, "w") as f:
+        f.write("different_content")
+
+    folder_comparison(source, replica)
+
+    assert os.path.isfile(replica_file1)  # File 1 is copied
+    assert os.path.isfile(replica_file2)  # File 2 is copied
+
+    # Check if content is the same
+    with open(source_file1, "r") as f1, open(replica_file1, "r") as f2:
+        assert f1.read() == f2.read()
+
+    with open(source_file2, "r") as f1, open(replica_file2, "r") as f2:
+        assert f1.read() == f2.read()
